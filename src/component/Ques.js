b6b629f8a138efb21291205613953f814d386d62
kishan955971 <kishan221314z@gmail.com>
2025-06-24 19:16:53 +0530

    project

--- src/component/Ques.js
+++ src/component/Ques.js
@@ -15,12 +15,9 @@ function Ques() {
         })
             .catch(error => {
                 console.log("Error", error);
-                
             });
     };
-
     const handleNext = (e) => {
-
         if (que < question.length - 1) 
             {
             setQue(que + 1);
@@ -29,31 +26,51 @@ function Ques() {
             alert("Question Finished");
         }
     };
-
+        const  handleb=()=>{
+            document.getElementById("e").checked=true;
+        }
     return (
-        <div>
-            <button onClick={show}>Start Quize</button>
+        <div className="col" >
+            <button onClick={show}  className="col2" >Start Quize</button>
             {question.length > 0 &&
-                <div>
-                    <h1>Question_No : {question[que]["ques_no"]}</h1>
-                    <h2>Question : {question[que]["question"]}</h2>
-                     <p>
-  <input type='radio' name="r"  /><b>A: {question[que]["a"]}</b></p>
-
-<p><input type='radio' name="r" /><b>B: {question[que]["b"]}</b></p>
-
-<p><input type='radio' name="r"/><b>C: {question[que]["c"]}</b></p>
-
-<p><input type='radio' name="r" /><b>D: {question[que]["d"]}</b></p>
-
-
+                <div className="options" >
                     
+                    <h1 className="que">Question_No : {question[que]["ques_no"]}</h1>
+                    <h2 className="ans" >Question : {question[que]["question"]}</h2>
+                
+                  <p>
+  <input type='radio' name="r" value="a" id="a" />
+  <b>A: {question[que]["a"]}</b>
+</p>
+
+<p>
+  <input type='radio' name="r" value="b" id="b" />
+  <b>B: {question[que]["b"]}</b>
+</p>
+
+<p> 
+  <input type='radio' name="r" value="c" id="c" />
+  <b>C: {question[que]["c"]}</b>
+</p>
+
+<p>
+  <input type='radio' name="r" value="d" id="d" />
+  <b>D: {question[que]["d"]}</b>
+</p>
+
+<p>
+  <input style={{display:"none"}} type='radio' name="r" value="e" id="e" />
+</p>
+
+<button onClick={()=>{handleb();handleNext();}} className="col1" >Next</button>
+
+<input className="im" type="image" src="pic/c1.jpg"/>
 
                 </div>
-            }
-            <button onClick={handleNext}>Next</button>
+ }
+            
+       </div>
 
-        </div>
-    )
+    )   
 }
 export default Ques;
