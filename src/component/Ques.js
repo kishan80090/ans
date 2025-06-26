cce97ba8ef45e61a967354829a5caec41e3515ec
kishan955971 <kishan221314z@gmail.com>
2025-06-26 15:00:24 +0530

    Projectfinal

--- src/component/Ques.js
+++ src/component/Ques.js
@@ -1,83 +1,123 @@
 import React, { useState } from "react";
 import './Quesdesign.css';
 import axios from "axios";
+
 function Ques() {
     const [question, setQuestion] = useState([]);
-    const [que, setQue] = useState("0");
-    // let que = 0;
+    const [que, setQue] = useState(0);
+    const [score, setScore] = useState(0);
+    const imagep =[
+
+"pic/c1.jpg",
+"pic/c2.jpg",
+"pic/c6.jpg",
+"pic/c7.jpg"
+
+    ];
+
+    let correctanswer = "";
+
+    //********************************* show function ***************************************************
     const show = (e) => {
         const baseURL = 'https://kishan80090.github.io/jsondata/products.json';
 
         axios.get(baseURL).then((response) => {
-
             setQuestion(response.data);
             setQue(0);
-        })
-            .catch(error => {
-                console.log("Error", error);
+            setScore(0);
+
+            const options = ["a", "b", "c", "d", "e"];
+            options.forEach(id => {
+                const opt = document.getElementById(id);
+                if (opt) opt.checked = false;
             });
+        })
+        .catch(error => {
+            console.log("Error", error);
+        });
     };
-    const handleNext = (e) =>
-         {
-        if (que < question.length -1) 
-            { 
-         let lastrd= document.getElementById("e");
-        //  alert(lastrd.checked);
-         if(lastrd.checked)
-         {
+
+    //************************************* handleNext function *******************************************
+    const handleNext = (e) => {
+        const selected = document.querySelector('input[name="r"]:checked');
+        if (!selected || selected.id === "e") {
             alert("Please select option");
             return;
-         }
-                
-            setQue(que + 1);
-            document.getElementById("e").checked=true;
-
         }
-        else {
-            alert("Question Finished");
+
+        let givenanswer = test();
+        correctanswer = question[que]["correct"];
+
+        if (givenanswer === correctanswer) {
+            setScore(score + 1);
+        }
+
+        if (que < question.length - 1) {
+            setQue(que + 1);
+            document.getElementById("e").checked = true;
+        } else {
+            alert("Question Finished\nYour Score: "
+                 + (score + (givenanswer === correctanswer ? 1 : 0)) 
+                 + "/" + question.length);
         }
     };
 
+    const test = (e) => {
+        if (document.getElementById("a").checked)
+             return "a";
+        if (document.getElementById("b").checked)
+             return "b";
+        if (document.getElementById("c").checked)
+             return "c";
+        if (document.getElementById("d").checked)
+             return "d";
+        return false;
+    };
+
+    //**************************************** Return ***************************************************
     return (
-        <div className="col" >
-            <button onClick={show}  className="col2" >Start Quize</button>
+        <div className="col">
+            <button onClick={show} className="col2">Start Quiz</button>
+
             {question.length > 0 &&
-                <div className="options" >
-                    
+                <div className="options">
+
                     <h1 className="que">Question_No : {question[que]["ques_no"]}</h1>
-                    <h3 className="ans" >Question : {question[que]["question"]}</h3>
-                
-                  <p>
-  <input type='radio' name="r" value="a" id="a" />
-  <b>A: {question[que]["a"]}</b>
-</p>
+                    <h3 className="ans">Question : {question[que]["question"]}</h3>
+                    <h4 className="score" >Your Right Scores: {score}/{question.length}</h4>
+{/* <img className="im" src="pic/c1.jpg" alt="quiz visual" /> */}
+                    <p>
+                        <label><input type='radio' name="r" value="a" id="a" />
+                        <b>A: {question[que]["a"]}</b></label>
+                    </p>
 
-<p>
-  <input type='radio' name="r" value="b" id="b" />
-  <b>B: {question[que]["b"]}</b>
-</p>
+                    <p>
+                        <label><input type='radio' name="r" value="b" id="b" />
+                        <b>B: {question[que]["b"]}</b></label>
+                    </p>
 
-<p> 
-  <input type='radio' name="r" value="c" id="c" />
-  <b>C: {question[que]["c"]}</b>
-</p>
+                    <p>
+                        <label><input type='radio' name="r" value="c" id="c" />
+                        <b>C: {question[que]["c"]}</b></label>
+                        
+                    </p>
 
-<p>
-  <input type='radio' name="r" value="d" id="d" />
-  <b>D: {question[que]["d"]}</b>
-</p>
+                    <p>
+                        <label><input type='radio' name="r" value="d" id="d" />
+                        <b>D: {question[que]["d"]}</b></label>
+                    </p>
 
-<p>
-  <input style={{display:"none"}} type='radio' name="r" value="e" id="e" />
-</p>
+                    <p>
+                        <input style={{ display: "none" }} type='radio' name="r" value="e" id="e" defaultChecked />
+                    </p>
 
-<button onClick={handleNext} className="col1" >Next</button>
+                    <button onClick={handleNext} className="col1">Next</button>
 
-<input className="im" type="image" src="pic/c2.jpg"/>
-
-    </div>
+                    <img className="im" type="image" src={imagep[que]} alt={'${que+1}'} />
+                </div>
+            }
+        </div>
+    );
 }
-       </div>
-    )   
-}
+
 export default Ques;
