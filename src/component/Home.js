d75e3567127dd453d12c221bb9f52e77e40d165d
kishan955971 <kishan221314z@gmail.com>
2025-06-30 23:47:02 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,23 +5,21 @@ function Home() {
 
     return(
         <div className="home" >
-            
+            <h1>||     Welcome     ||</h1>
             <div className="button-container" >
 
         <Link to="/ques">
         <button className="home-button"><strong>c++</strong></button>
         </Link>
 
-        {/* <Link to="/java">
+        <Link to="/java">
         <button className="home-button"><strong>java</strong></button>
-        </Link> */}
+        </Link>
 
         
-        {/* <Link to="/phython">
+        <Link to="/phython">
         <button className="home-button"><strong>Phython</strong></button>
-        </Link> */}
-        
-
+        </Link>
 
 </div>
         </div>
